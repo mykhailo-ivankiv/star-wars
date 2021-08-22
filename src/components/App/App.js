445ab9cdc048ed5145507075d7e05e2b80b524ec
mivankiv <mivankiv@degiro.nl>
2021-08-22 12:13:59 +0200

    Initial configuration.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import BEM from "../../utils/ BEM.js"
+const b = BEM("App")
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Factris Technical Assignment
-          </p>
-        </header>
+      <div className={b()}>
+        <h1>Factris Technical Assignment</h1>
       </div>
     );
   }
---
+++ src/utils/ BEM.js
@@ -0,0 +1,24 @@
+const ELEMENT_SEPARATOR = "__"
+const MODIFIER_SEPARATOR = "_"
+
+const getBEMPAth = (b, e, m) => {
+  const base = e !== "" ? [b, e].join(ELEMENT_SEPARATOR) : b
+
+  return `${base} ${m.reduce((str, m) => `${str} ${base}${MODIFIER_SEPARATOR}${m}`, "")}`
+}
+
+const b = (b) => (elementName, modifiers = []) => {
+  modifiers = typeof elementName === "string" ? modifiers : elementName || []
+
+  return getBEMPAth(
+    b,
+    typeof elementName === "string" ? elementName : "",
+    Array.isArray(modifiers)
+      ? modifiers.filter(Boolean)
+      : Object.entries(modifiers)
+        .filter(([, value]) => Boolean(value))
+        .map(([key]) => key)
+  )
+}
+
+export default b
