f6e33eea44708f7f2dacbe12d5a2605fb10ab6e3
mivankiv <mivankiv@degiro.nl>
2021-08-22 12:23:27 +0200

    Add prettier

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import BEM from "../../utils/ BEM.js"
-const b = BEM("App")
+import React from "react";
+import "./App.css";
+import BEM from "../../utils/ BEM.js";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <div className={b()}>
-        <h1>Factris Technical Assignment</h1>
-      </div>
-    );
-  }
-}
+const b = BEM("App");
+
+console.log(axios.get("/people"));
+
+const App = () => {
+  return (
+    <div className={b()}>
+      <h1>Factris Technical Assignment</h1>
+    </div>
+  );
+};
 
 export default App;
